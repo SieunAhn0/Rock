4903772bfaaa8c2297c5a89187df4db3dcc16783
SieunAhn0 <saahn1405@gmail.com>
2026-09-05 01:40:28 -0700

    Added spawning droplets
    Created scripts and prefabs folders, and added basic script for droplets to spawn every few seconds.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject[] gameObjects;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Invoke("Spawn", 1f);
+    }
+
+    void Spawn() {
+        GameObject randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
+        Instantiate(randomObject, transform.position, Quaternion.identity);
+        Invoke("Spawn", 2f);
+    }
+}
